306dd6d8bf1765b1c122bb22dcf0692b13232e8d
Ergi <ergimandija1@gmail.com>
2026-06-19 10:13:06 +0200

    created test for quality not to be negative

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,4 +6,10 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(2);
   });
 
+  it("should never give out negative quality", function() {
+    items = [ new Item("testitem", 0, 0) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
 });
