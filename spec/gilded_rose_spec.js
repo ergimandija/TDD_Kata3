d6186759afd42ad47e0d2f34aa4dcdb2679efbd6
Ergi <ergimandija1@gmail.com>
2026-06-19 10:41:43 +0200

    create test for backstage passes AI-> create the subtests for the different cases

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -25,11 +25,31 @@ describe("Gilded Rose", function() {
     expect(items[1].quality).toEqual(50);
   });
 
-  
-   it("should never change the quality of Sulfuras", function() {
+    
+  it("should never change Sulfuras quality or sell_in across multiple updates", function() {
     items = [ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ];
     update_quality();
+    update_quality();
+    update_quality();
     expect(items[0].quality).toEqual(80);
+    expect(items[0].sell_in).toEqual(0);
+  });
+    it("should increase quality as sell_in approaches and drop to 0 after concert", function() {
+    items = [ new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(21);
+
+    items = [ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(22);
+
+    items = [ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(23);
+
+    items = [ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
   });
 
 });
